49fad56be7933946ed40e52a1f44e7c66cd68b65
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-04-15 20:35:54 -0700

    Campsite.cs finished

--- COMP003B.Assignment4/Models/Campsite.cs
+++ COMP003B.Assignment4/Models/Campsite.cs
@@ -9,4 +9,15 @@ public class Campsite
     [Required]
     [StringLength(100, ErrorMessage = "Name cannot be longer than 100 charecters.")]
     public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(100, ErrorMessage = "Location can not be longer tha 100 charecters.")]
+    public string Location { get; set; } = string.Empty;
+    
+    [Required]
+    [Range(1, 16, ErrorMessage = "Max Occupancy must be between 1 and 20.")]
+    public int MaxOccupancy { get; set; }
+    
+    [Required]
+    [Range(1.00, 300.00, ErrorMessage = "Price per night must be between $1.00 and $300.00.")]
 }
