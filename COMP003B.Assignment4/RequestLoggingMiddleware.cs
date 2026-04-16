9b6245832d9921a4b659882e83fdbac034007fb0
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-04-15 22:29:56 -0700

    fixed missing ] and typo on invoke

--- COMP003B.Assignment4/RequestLoggingMiddleware.cs
+++ COMP003B.Assignment4/RequestLoggingMiddleware.cs
@@ -9,9 +9,9 @@ public class RequestLoggingMiddleware
         _next = next;
     }
 
-    public async Task InvkeAsync(HttpContext context)
+    public async Task InvokeAsync(HttpContext context)
     {
-        Console.WriteLine($"[{DateTime.Now} {context.Request.Method} {context.Request.Path}");
+        Console.WriteLine($"[{DateTime.Now}] {context.Request.Method} {context.Request.Path}");
         await _next(context);
     }
     
